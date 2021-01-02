d36dceb404e34f9467dfc8179d6e10a2885e80cc
Deep Majumder <deep.majumder2019@gmail.com>
2021-01-02 15:59:20 +0530

    Added NewTree and extendNode functions

--- afs/afs.go
+++ afs/afs.go
@@ -1,5 +1,12 @@
 package afs
 
+import (
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+)
+
 // An Abstract File System which mimics a file system tree
 // but is created to store information about paths and their corresponding
 // Google Drive ID.
@@ -35,6 +42,51 @@ func newNode(name, parent string, isDir, isRecursive bool) *Node {
 	}
 }
 
+func (node *Node) fullPath() string {
+	return path.Join(node.parent, node.name)
+}
+
+// Explores a node, to extend it downwards
+// Nothing to explore if it's a file
+// If it's a directory and is non-recursive, add all its files (but not directories) as children
+// It it's a directory and is recursive, add all its files and recursively explore its sub-directories
+func (node *Node) extendNode() {
+	if node.isDir {
+		currPath := node.fullPath()
+		dir, err := os.Open(currPath)
+		defer dir.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		contents, err := dir.Readdirnames(-1)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, name := range contents {
+			newPath := path.Join(currPath, name)
+			if stat, err := os.Stat(newPath); os.IsExist(err) {
+				newIsDir := stat.IsDir()
+				newNode := newNode(name, currPath, newIsDir, node.isRecursive)
+				node.children[name] = newNode
+				if node.isRecursive {
+					newNode.extendNode()
+				}
+			}
+		}
+	}
+}
+
 // NewTree creates a new tree from a given directory
-// func NewTree(dir string) *Tree {
-// }
+func NewTree(dir string, isRecursive bool) *Tree {
+	parts := splitPath(dir, string(filepath.Separator))
+	parent := joinPath(parts[:len(parts)-1], string(filepath.Separator), true)
+	dirName := parts[len(parts)-1]
+
+	rootNode := newNode(parent, dirName, true, isRecursive)
+	rootNode.extendNode()
+
+	return &Tree{
+		name: parent,
+		root: rootNode,
+	}
+}
