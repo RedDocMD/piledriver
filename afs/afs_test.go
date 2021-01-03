edb00406c7d57d8d18c4a861ce3005b124466e3f
Deep Majumder <deep.majumder2019@gmail.com>
2021-01-03 16:36:44 +0530

    Added tests for AddPath and findPath

--- afs/afs_test.go
+++ afs/afs_test.go
@@ -3,7 +3,12 @@ package afs
 import (
 	"fmt"
 	"log"
+	"os"
+	"path"
 	"path/filepath"
+	"testing"
+
+	"github.com/alecthomas/assert"
 )
 
 func ExampleNewTree() {
@@ -13,4 +18,75 @@ func ExampleNewTree() {
 	}
 	tree := NewTree(path, true)
 	fmt.Println(tree)
+}
+
+func TestFindPath(t *testing.T) {
+	path, err := filepath.Abs("test_data/rec_dir")
+	assert := assert.New(t)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tree := NewTree(path, true)
+
+	_, found := tree.findPath(path)
+	assert.True(found)
+
+	_, found = tree.findPath(filepath.Join(path, "dir1/dir3/file6"))
+	assert.True(found)
+
+	_, found = tree.findPath(filepath.Join(path, "dir2/file7"))
+	assert.True(found)
+
+	_, found = tree.findPath(filepath.Join(path, "file3"))
+	assert.True(found)
+
+	_, found = tree.findPath(filepath.Join(path, "file10"))
+	assert.False(found)
+
+	_, found = tree.findPath(filepath.Join(path, "dir11/file10"))
+	assert.False(found)
+
+	_, found = tree.findPath(filepath.Join(path, "dir1/dir3/file22"))
+	assert.False(found)
+}
+
+func TestAddPath(t *testing.T) {
+	assert := assert.New(t)
+	tempDir := os.TempDir()
+	if _, err := os.Stat(tempDir); !os.IsNotExist(err) {
+		basePath := path.Join(tempDir, "test")
+		err = os.Mkdir(basePath, os.ModePerm)
+		if err != nil {
+			t.Error(err)
+		}
+
+		tree := NewTree(basePath, true)
+
+		newPath := path.Join(basePath, "dir1/dir2")
+		err = os.MkdirAll(newPath, os.ModePerm)
+		if err != nil {
+			t.Error(err)
+		}
+		tree.AddPath(newPath, true)
+		node, ok := tree.findPath(newPath)
+		assert.True(ok)
+		assert.True(node.isDir)
+		assert.Equal(node.name, "dir2")
+
+		newPath = path.Join(basePath, "dir1/file")
+		file, err := os.Create(newPath)
+		defer file.Close()
+		if err != nil {
+			t.Error(err)
+		}
+		tree.AddPath(newPath, false)
+		node, ok = tree.findPath(newPath)
+		assert.True(ok)
+		assert.False(node.isDir)
+		assert.Equal(node.name, "file")
+
+		os.RemoveAll(basePath)
+	} else {
+		t.Error("Cannot access tmp directory")
+	}
 }
