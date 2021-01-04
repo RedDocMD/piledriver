9e7c149b63638153fb9c6f8553e931dc0f1acce6
Deep Majumder <deep.majumder2019@gmail.com>
2021-01-04 09:22:57 +0530

    Made test paths platform independent

--- afs/afs_test.go
+++ afs/afs_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 func ExampleNewTree() {
-	path, err := filepath.Abs("test_data/rec_dir")
+	path, err := filepath.Abs(filepath.FromSlash("test_data/rec_dir"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,7 +21,7 @@ func ExampleNewTree() {
 }
 
 func TestFindPath(t *testing.T) {
-	path, err := filepath.Abs("test_data/rec_dir")
+	path, err := filepath.Abs(filepath.FromSlash("test_data/rec_dir"))
 	assert := assert.New(t)
 	if err != nil {
 		log.Fatal(err)
@@ -31,10 +31,10 @@ func TestFindPath(t *testing.T) {
 	_, found := tree.findPath(path)
 	assert.True(found)
 
-	_, found = tree.findPath(filepath.Join(path, "dir1/dir3/file6"))
+	_, found = tree.findPath(filepath.Join(path, filepath.FromSlash("dir1/dir3/file6")))
 	assert.True(found)
 
-	_, found = tree.findPath(filepath.Join(path, "dir2/file7"))
+	_, found = tree.findPath(filepath.Join(path, filepath.FromSlash("dir2/file7")))
 	assert.True(found)
 
 	_, found = tree.findPath(filepath.Join(path, "file3"))
@@ -43,10 +43,10 @@ func TestFindPath(t *testing.T) {
 	_, found = tree.findPath(filepath.Join(path, "file10"))
 	assert.False(found)
 
-	_, found = tree.findPath(filepath.Join(path, "dir11/file10"))
+	_, found = tree.findPath(filepath.Join(path, filepath.FromSlash("dir11/file10")))
 	assert.False(found)
 
-	_, found = tree.findPath(filepath.Join(path, "dir1/dir3/file22"))
+	_, found = tree.findPath(filepath.Join(path, filepath.FromSlash("dir1/dir3/file22")))
 	assert.False(found)
 }
 
@@ -62,7 +62,7 @@ func TestAddPath(t *testing.T) {
 
 		tree := NewTree(basePath, true)
 
-		newPath := path.Join(basePath, "dir1/dir2")
+		newPath := path.Join(basePath, filepath.FromSlash("dir1/dir2"))
 		err = os.MkdirAll(newPath, os.ModePerm)
 		if err != nil {
 			t.Error(err)
@@ -73,7 +73,7 @@ func TestAddPath(t *testing.T) {
 		assert.True(node.isDir)
 		assert.Equal(node.name, "dir2")
 
-		newPath = path.Join(basePath, "dir1/file")
+		newPath = path.Join(basePath, filepath.FromSlash("dir1/file"))
 		file, err := os.Create(newPath)
 		defer file.Close()
 		if err != nil {
@@ -92,53 +92,54 @@ func TestAddPath(t *testing.T) {
 }
 
 func TestDeletePath(t *testing.T) {
-	path, err := filepath.Abs("test_data/rec_dir")
+	path, err := filepath.Abs(filepath.FromSlash("test_data/rec_dir"))
 	assert := assert.New(t)
 	if err != nil {
 		log.Fatal(err)
 	}
 	tree := NewTree(path, true)
 
-	_, found := tree.findPath(filepath.Join(path, "dir1/dir3/file6"))
+	_, found := tree.findPath(filepath.Join(path, filepath.FromSlash("dir1/dir3/file6")))
 	assert.True(found)
-	tree.DeletePath(filepath.Join(path, "dir1/dir3/file6"))
-	_, found = tree.findPath(filepath.Join(path, "dir1/dir3/file6"))
+	tree.DeletePath(filepath.Join(path, filepath.FromSlash("dir1/dir3/file6")))
+	_, found = tree.findPath(filepath.Join(path, filepath.FromSlash("dir1/dir3/file6")))
 	assert.False(found)
 
-	_, found = tree.findPath(filepath.Join(path, "dir2/file7"))
+	_, found = tree.findPath(filepath.Join(path, filepath.FromSlash("dir2/file7")))
 	assert.True(found)
 	tree.DeletePath(filepath.Join(path, "dir2"))
 	_, found = tree.findPath(filepath.Join(path, "dir2"))
 	assert.False(found)
-	_, found = tree.findPath(filepath.Join(path, "dir2/file7"))
+	_, found = tree.findPath(filepath.Join(path, filepath.FromSlash("dir2/file7")))
 	assert.False(found)
 }
 
 func TestRenamePath(t *testing.T) {
-	path, err := filepath.Abs("test_data/rec_dir")
+	path, err := filepath.Abs(filepath.FromSlash("test_data/rec_dir"))
 	assert := assert.New(t)
 	if err != nil {
 		log.Fatal(err)
 	}
 	tree := NewTree(path, true)
 
-	_, found := tree.findPath(filepath.Join(path, "dir1/dir3/file6"))
+	_, found := tree.findPath(filepath.Join(path, filepath.FromSlash("dir1/dir3/file6")))
 	assert.True(found)
-	tree.RenamePath(filepath.Join(path, "dir1/dir3/file6"), filepath.Join(path, "dir1/dir3/newfile"))
+	tree.RenamePath(filepath.Join(path, filepath.FromSlash("dir1/dir3/file6")),
+		filepath.Join(path, filepath.FromSlash("dir1/dir3/newfile")))
 	_, found = tree.findPath(filepath.Join(path, "dir1/dir3/newfile"))
 	assert.True(found)
-	_, found = tree.findPath(filepath.Join(path, "dir1/dir3/file6"))
+	_, found = tree.findPath(filepath.Join(path, filepath.FromSlash("dir1/dir3/file6")))
 	assert.False(found)
 
-	_, found = tree.findPath(filepath.Join(path, "dir2/file7"))
+	_, found = tree.findPath(filepath.Join(path, filepath.FromSlash("dir2/file7")))
 	assert.True(found)
 	tree.RenamePath(filepath.Join(path, "dir2"), filepath.Join(path, "dirnew"))
 	_, found = tree.findPath(filepath.Join(path, "dir2"))
 	assert.False(found)
-	_, found = tree.findPath(filepath.Join(path, "dir2/file7"))
+	_, found = tree.findPath(filepath.Join(path, filepath.FromSlash("dir2/file7")))
 	assert.False(found)
 	_, found = tree.findPath(filepath.Join(path, "dirnew"))
 	assert.True(found)
-	_, found = tree.findPath(filepath.Join(path, "dirnew/file7"))
+	_, found = tree.findPath(filepath.Join(path, filepath.FromSlash("dirnew/file7")))
 	assert.True(found)
 }
